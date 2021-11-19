431cc290fd4c8810e0ac8cf5893b79e8137e5124
Antti K. Koskela <posti@koskila.net>
2021-11-19 09:24:41 +0200

    Deebot version update

--- HomeAssistant/custom_components/deebot/const.py
+++ HomeAssistant/custom_components/deebot/const.py
@@ -1,3 +1,4 @@
+"""Const module."""
 from deebotozmo.models import VacuumState
 from homeassistant.components.vacuum import (
     STATE_CLEANING,
@@ -9,8 +10,14 @@ from homeassistant.components.vacuum import (
 )
 from homeassistant.const import CONF_PASSWORD, CONF_USERNAME, CONF_VERIFY_SSL
 
+################################
+# Do not change! Will be set by release workflow
+INTEGRATION_VERSION = "3.2.1"
+MIN_REQUIRED_HA_VERSION = "2021.9.0b0"
+################################
+
+# Values below can be changed
 DOMAIN = "deebot"
-INTEGRATION_VERSION = "3.1.0"
 ISSUE_URL = "https://github.com/And3rsL/Deebot-for-Home-Assistant/issues"
 
 STARTUP_MESSAGE = f"""
@@ -25,19 +32,10 @@ If you have any issues with this you need to open an issue here:
 
 CONF_COUNTRY = "country"
 CONF_CONTINENT = "continent"
-DEEBOT_DEVICES = f"{DOMAIN}_devices"
-VACUUMSTATE_TO_STATE = {
-    VacuumState.STATE_IDLE: STATE_IDLE,
-    VacuumState.STATE_CLEANING: STATE_CLEANING,
-    VacuumState.STATE_RETURNING: STATE_RETURNING,
-    VacuumState.STATE_DOCKED: STATE_DOCKED,
-    VacuumState.STATE_ERROR: STATE_ERROR,
-    VacuumState.STATE_PAUSED: STATE_PAUSED,
-}
-
 CONF_BUMPER = "Bumper"
 CONF_MODE_BUMPER = CONF_BUMPER
 CONF_MODE_CLOUD = "Cloud (recommended)"
+CONF_CLIENT_DEVICE_ID = "client_device_id"
 
 # Bumper has no auth and serves the urls for all countries/continents
 BUMPER_CONFIGURATION = {
@@ -45,7 +43,18 @@ BUMPER_CONFIGURATION = {
     CONF_COUNTRY: "it",
     CONF_PASSWORD: CONF_BUMPER,
     CONF_USERNAME: CONF_BUMPER,
-    CONF_VERIFY_SSL: False  # required as bumper is using self signed certificates
+    CONF_VERIFY_SSL: False,  # required as bumper is using self signed certificates
+}
+
+DEEBOT_DEVICES = f"{DOMAIN}_devices"
+
+VACUUMSTATE_TO_STATE = {
+    VacuumState.IDLE: STATE_IDLE,
+    VacuumState.CLEANING: STATE_CLEANING,
+    VacuumState.RETURNING: STATE_RETURNING,
+    VacuumState.DOCKED: STATE_DOCKED,
+    VacuumState.ERROR: STATE_ERROR,
+    VacuumState.PAUSED: STATE_PAUSED,
 }
 
 LAST_ERROR = "last_error"
@@ -60,3 +69,5 @@ EVENT_STATS = "Stats"
 EVENT_LIFE_SPAN = "Life spans"
 EVENT_ROOMS = "Rooms"
 EVENT_MAP = "Map"
+
+EVENT_CUSTOM_COMMAND = "deebot_custom_command"
